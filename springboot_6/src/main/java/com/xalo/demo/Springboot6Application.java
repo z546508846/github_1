5146954892061c0ea31677c2a5b1a07f38cde33c
XuanJx <546508846@qq.com>
2019-08-15 18:00:33 +0800

    111

---
+++ springboot_6/src/main/java/com/xalo/demo/Springboot6Application.java
@@ -0,0 +1,13 @@
+package com.xalo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot6Application.class, args);
+    }
+
+}
