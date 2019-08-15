c580934dcf1d28a7fa08f37939b90c108d3c2b36
XuanJx <546508846@qq.com>
2019-08-15 15:45:20 +0800

    111

---
+++ springboot_6/src/main/java/com/xalo/demo/Student.java
@@ -0,0 +1,38 @@
+package com.xalo.demo;
+
+/**
+ * @Auther:
+ * @Date: 2019/8/15 11:29
+ * @Description:
+ */
+
+public class Student {
+
+
+    private Integer id;
+    private String name ;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
